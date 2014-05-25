4d79d3319df631da264323fb4c5cfb71f89e30d1
ghubrazil <lishibashy@yahoo.com.br>
2014-05-24 23:35:15 -0300

    Update Test

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
+## The two functions below are used to cache the inverse of a matrix.
 
 ## Write a short comment describing this function
+## The function makeCacheMatrix creates a list containing a function to set and get 
+## the value of the matrix and the value of inverse of the matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
